f88f558202339f0e01bd0936e151d0408bc30517
arn <arnaud@desaivres.net>
2020-03-01 18:01:07 +0100

    add time in config

--- hydro_poller.rb
+++ hydro_poller.rb
@@ -25,6 +25,8 @@ end
 config = YAML.load_file('config.yml')
 INFLUXDBHOST = config['influxdb']['host']
 INFLUXDBDATABASE = config['influxdb']['database']
+POLLINGINTERVAL = config['pollinginterval']
+INTERVAL = config['interval']
 
 # Set Log Level
 LOGLEVEL ||= Logger::DEBUG
@@ -42,44 +44,45 @@ end
 HUBEAU_CONFIGURATION = config['hubeau']
 @hubeau = Hubeau.new HUBEAU_CONFIGURATION
 
-period = Time.new - 7200
-departements = config['hubeau']['departement'].join(',')
-sites_list = @hubeau.getSites(code_departement: departements)
-sites_list.each do |site|
-	puts site['libelle_site']
-	stations_list = @hubeau.getStations(code_site: site['code_site'])
-	stations_list.each do |station|
-	    puts station['libelle_station']
-	    observations_tr = @hubeau.getObservations_tr(code_entite: station['code_station'], date_debut_obs: period.strftime('%Y-%m-%dT%H:%M:%S.%L%z'))
-	    puts observations_tr.length
-	    observations_tr.each do |observation|
-	
-		    payload = {
-			    "values": {
-				    "epoch":	 		Time.iso8601(observation['date_obs']).to_i,
-				    "resultat_obs": 		observation['resultat_obs'],
-				    "longitude_station":	station['longitude_station'],
-				    "latitude_station":		station['latitude_station'],
-			    },
-			    "timestamp": Time.iso8601(observation['date_obs']).to_i,
-			    "tags": {
-				    "source": 			"hubeau",
-				    "libelle_station": 		station['libelle_station'],
-				    "libelle_site":		site['libelle_site'],
-				    "libelle_commune":		station['libelle_commune'],
-				    "libelle_departement": 	station['libelle_departement'],
-				    "code_departement":		station['code_departement'],
-				    "libelle_cours_eau": 	station['libelle_cours_eau'] || station['libelle_cours_eau'] = "Null",
-				    "code_region": 		station['code_region'],
-				    "code_station": 		station['code_station'],
-				    "grandeur_hydro": 		observation['grandeur_hydro'],
-				    "libelle_region":		station['libelle_region'],
-			    },
-		    }
-	
-		    updatehydro(payload,station['libelle_region'])
-		    sleep(0.5)
-	
-	    end
+period = Time.new - INTERVAL
+while true
+	departements = config['hubeau']['departement'].join(',')
+	sites_list = @hubeau.getSites(code_departement: departements)
+	sites_list.each do |site|
+
+		stations_list = @hubeau.getStations(code_site: site['code_site'])
+		stations_list.each do |station|
+		    observations_tr = @hubeau.getObservations_tr(code_entite: station['code_station'], date_debut_obs: period.strftime('%Y-%m-%dT%H:%M:%S.%L%z'))
+		    $log.info "Polling "+station['code_departement']+" Site: " +  site['libelle_site'] + " - Station: " + station['libelle_station'] + " ("+station['code_station']+"): " + observations_tr.length.to_s + " Observations" 
+		    observations_tr.each do |observation|
+		
+			    payload = {
+				    "values": {
+					    "epoch":	 		Time.iso8601(observation['date_obs']).to_i,
+					    "resultat_obs": 		observation['resultat_obs'],
+					    "longitude_station":	station['longitude_station'],
+					    "latitude_station":		station['latitude_station'],
+				    },
+				    "timestamp": Time.iso8601(observation['date_obs']).to_i,
+				    "tags": {
+					    "source": 			"hubeau",
+					    "libelle_station": 		station['libelle_station'],
+					    "libelle_site":		site['libelle_site'],
+					    "libelle_commune":		station['libelle_commune'],
+					    "libelle_departement": 	station['libelle_departement'],
+					    "code_departement":		station['code_departement'],
+					    "libelle_cours_eau": 	station['libelle_cours_eau'] || station['libelle_cours_eau'] = "Null",
+					    "code_region": 		station['code_region'],
+					    "code_station": 		station['code_station'],
+					    "grandeur_hydro": 		observation['grandeur_hydro'],
+					    "libelle_region":		station['libelle_region'],
+				    },
+			    }
+		
+			    updatehydro(payload,station['libelle_region'])
+			    sleep(0.8)
+		    end
+		end
 	end
+	sleep(POLLINGINTERVAL)
 end
